56ea61e56837f74762ed21abc6373669981e57de
Krishbin Paudel <krishbin@onmail.com>
2022-11-03 01:05:11 +0545

    api-added: interface to /update to get data
    added a new table device and changed the relational shape of the data.
    Now Device should be registered initially through /registerdevice with a
    serial number. Then user can be associated with the device and each
    request from the user is stored in Location with the device id.
    /update exposes an uri through which we are able to request the pi to
    fetch any incoming data. On receiving an incoming data, js forwards with
    the location data to a new page.

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from datetime import datetime
 from peewee import *
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import os
-# from playhouse.migrate import *
 
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
+from datetime import datetime
+import json
+
+RADIO_FREQ_MHZ = 433.0  # Frequency of the radio in Mhz. Must match your
+CS = digitalio.DigitalInOut(board.CE1)
+RESET = digitalio.DigitalInOut(board.D25)
+LED = digitalio.DigitalInOut(board.D13)
+LED.direction = digitalio.Direction.OUTPUT
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+rfm9x.tx_power = 23
 
 db = SqliteDatabase("users.db")
 app = Flask(__name__)
-# migrator = SqliteMigrator(db)
 
 UPLOAD_FOLDER = 'static/img/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# migrate(
-#     migrator.add_column('user', 'title', title_field),
-#     migrator.add_column('some_table', 'status', status_field),
-#     migrator.drop_column('some_table', 'old_column'),
-# )
  
 class BaseTable(Model):
     class Meta:
         database = db
+
+class Device(BaseTable):
+    serial_number = IntegerField(unique=True)
+    occupied = BooleanField()
+
 class User(BaseTable):
     # id = PrimaryKeyField()
     name = TextField()
@@ -30,31 +43,23 @@ class User(BaseTable):
     Country = TextField()
     fileName= TextField()
     EmergencyContact = TextField()
-    DeviceID = IntegerField(unique=True, null=False)
-    
-    class Meta:
-        database = db
-
+    DeviceID = ForeignKeyField(Device, backref="id")
 
 class Location(BaseTable):
-    UserID = ForeignKeyField(User, backref="id")
+    DeviceID = ForeignKeyField(Device, backref="id")
     Latitude = FloatField()
     Longitude = FloatField()
     LastUpdate = DateTimeField(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    class Meta:
-        database = db
 
 # Connection to database and create tables
 db.connect()
-db.create_tables([User, Location])
+db.create_tables([User,Device, Location])
 
 # Index page
 @app.route('/')
 def dashboard():
-    # Get all users
     users = User.select()
     return render_template('dashboard.html', users=users)
-
 
 # Register user route
 @app.route('/register',methods=['GET','POST'])
@@ -73,21 +78,56 @@ def register():
         return redirect(url_for('dashboard'))
     return render_template('register.html')
 
+# Register user route
+@app.route('/registerdevice',methods=['GET','POST'])
+def registerdevice():
+    if request.method == 'POST':
+        devid = request.form.get('dev-id')
+        occupied = False
+        Device.create(id=1,serial_number=devid, occupied=occupied)
+        return redirect(url_for('dashboard'))
+    return render_template('register-device.html')
+
 # Profile Route
 @app.route('/profile/<int:device>')
 def profile(device):
     user = User.get(User.DeviceID == device)
-    lat = 28.5300
-    long = 83.8780
-    return render_template('profile.html', user=user, lat=lat, long=long)
+    try:
+        location = Location.get(Location.DeviceID == device)
+        lat = location.Latitude
+        lon = location.Longitude
+    except:
+        lat = 23.37
+        lon = 87.67
+    return render_template('profile.html', user=user, lat=lat, long=lon)
 
 @app.route('/delete/<username>')
 def delete(username):
     obj=User.get(User.name==username)
     obj.delete_instance()
     return redirect(url_for('dashboard'))
-    
 
+@app.route('/update')
+def update():
+    packet = rfm9x.receive()
+    if packet is not None:
+        LED.value = True
+        rssi = rfm9x.last_rssi
+        try:
+            packet_text = str(packet,"ascii")
+        except:
+            return jsonify({})
+        packet_json = json.loads(packet_text)
+        try:
+            print(packet_json["l"])
+            loc = Location.get(Location.Latitude == packet_json["l"][0])
+        except:
+            Location.create(DeviceID = packet_json["i"], Latitude = packet_json["l"][0], Longitude = packet_json["l"][1])
+        LED.value = False
+        return jsonify(packet_json)
+    LED.value = False
+    return jsonify({})
+        
 # server
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1',port=5000)
+    app.run(debug=True, host='0.0.0.0',port=8000)
