621bce01cbf96d5d9d636f313d5734628fb0ee94
Bhairu Tribrid <bhairutribrid@gmail.com>
2022-11-02 20:31:49 +0545

    delete user from dashboard added

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template, url_for, redirect, request
 from datetime import datetime
 from peewee import *
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import os
+# from playhouse.migrate import *
+
 
 db = SqliteDatabase("users.db")
 app = Flask(__name__)
+# migrator = SqliteMigrator(db)
+
+UPLOAD_FOLDER = 'static/img/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# migrate(
+#     migrator.add_column('user', 'title', title_field),
+#     migrator.add_column('some_table', 'status', status_field),
+#     migrator.drop_column('some_table', 'old_column'),
+# )
     
-class User(Model):
+class BaseTable(Model):
+    class Meta:
+        database = db
+class User(BaseTable):
     name = TextField()
     age = IntegerField()
-    StartDate = TextField(default=datetime.now())
+    StartDate = TextField(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     Country = TextField()
+    fileName= TextField()
     EmergencyContact = TextField()
     DeviceID = IntegerField(primary_key=True, unique=True, null=False)
+    
     class Meta:
         database = db
 
-class Location(Model):
+class Location(BaseTable):
     DeviceID = ForeignKeyField(User, backref="DeviceID")
     Latitude = FloatField()
     Longitude = FloatField()
@@ -34,6 +53,7 @@ def dashboard():
     users = User.select()
     return render_template('dashboard.html', users=users)
 
+
 # Register user route
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -43,7 +63,11 @@ def register():
         country = request.form.get('country')
         emergency = request.form.get('contact')
         device = request.form.get('device')
-        User.create(name=name, age=age, Country=country, EmergencyContact=emergency, DeviceID=device)
+        file=request.files['file']
+        filename=secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        print(name, age, country, emergency, device,filename)
+        User.create(name=name, age=age, Country=country, EmergencyContact=emergency, DeviceID=device,fileName=filename)
         return redirect(url_for('dashboard'))
     return render_template('register.html')
 
@@ -52,6 +76,13 @@ def register():
 def profile(age):
     user = User.get(User.age == age)
     return render_template('profile.html', user=user)
+
+@app.route('/delete/<username>')
+def delete(username):
+    obj=User.get(User.name==username)
+    obj.delete_instance()
+    return redirect(url_for('dashboard'))
+    
 
 # server
 if __name__ == '__main__':
